96e4d72fb0249775a738e3dc032128d6d1747257
Voidbrr <void.10677@gmail.com>
2026-01-11 12:06:26 +0500

    InputSection.tsx

---
+++ components/InputSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InputSection = () => {
+  return (
+    <div className="input-container">
+      <input type="text" placeholder="Enter your text here..." />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default InputSection;
